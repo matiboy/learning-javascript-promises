8a1fd357d6c6c3cf9d4fd91ae211960e8e6e4dca
matiboy <mathieu.chauvinc@gmail.com>
2015-08-06 07:44:46 +0800

    Tv widget

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-angular.module('SmashBoard', [])
-.controller('TvController', function($scope) {
-    // API Url - Replace "now" with the current unix timestamp
-    // var url = 'http://redape.cloudapp.net/tvguidea/singleslot/'+now+'?channels=[1,159,63,64]&format=json&o=1'
+angular.module('SmashBoard', []).controller('TvController', function($scope, $http) {
+  var now = Math.floor(new Date().getTime() / 1000);
+  var url = 'http://redape.cloudapp.net/tvguidea/singleslot/'+now+'?channels=[1,159,63,64]&format=json&o=1'
+  var ajaxPromise = $http.get(url);
+  ajaxPromise.then(function weGotData(response) {
+    $scope.channels = response.data.events;
+  });
 });
