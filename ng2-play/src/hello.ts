c2cc7d9ac3ce79b99f7af2dd24dcbe586bf22952
matiboy <mathieu.chauvinc@gmail.com>
2015-10-17 15:09:49 +0800

    Name change

--- ng2-play/src/hello.ts
+++ ng2-play/src/hello.ts
@@ -9,7 +9,13 @@ import {Component, bootstrap} from 'angular2/angular2';
 export class HelloApp {
     name: string = 'World';
     constructor() {
-
+      var self = this;
+      fetch('http://jsonplaceholder.typicode.com/users/1')
+        .then(function(response) {
+          response.json().then(function(obj) {
+            self.name = obj.name;
+          });
+        })
     }
 }
 
