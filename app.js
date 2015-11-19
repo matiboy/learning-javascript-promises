e895662388e18b25719cda18f80ca4659332eeff
matiboy <mathieu.chauvinc@gmail.com>
2015-11-20 07:10:32 +0800

    Correct url

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function getGeolocation() {
 
 angular.module('SmashBoard', []).controller('TvController', function($scope, $http) {
   var now = Math.floor(new Date().getTime() / 1000);
-  var url = 'http://companionapi.pink.cat/epgapi/singleslot/'+now+'?channels=[1,159,63,64]&format=json&o=1'
+  var url = 'http://redape.cloudapp.net/tvguidea/singleslot/'+now+'?channels=[1,159,63,64]&format=json&o=1'
   var ajaxPromise = $http.get(url);
   ajaxPromise.then(function weGotData(response) {
     $scope.channels = response.data.events;
