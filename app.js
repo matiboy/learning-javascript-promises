f4a4876c34344bdb7ce56b602f802f533230b297
matiboy <mathieu.chauvinc@gmail.com>
2015-11-20 06:49:28 +0800

    Include API url as a comment

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 angular.module('SmashBoard', [])
 .controller('TvController', function($scope) {
-
+    // API Url - Replace "now" with the current unix timestamp
+    // var url = 'http://redape.cloudapp.net/tvguidea/singleslot/'+now+'?channels=[1,159,63,64]&format=json&o=1'
 });
